53cffc1806be700d293684f3973853b56b0cee9b
alexa <alexa@DESKTOP-OPTF0E2>
2020-09-06 00:56:53 +0300

    Second Commit

---
+++ one.js
@@ -0,0 +1,7 @@
+var isDone = false;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var big = 100n;
+console.log("Hello TypeScript!");
--- one.ts
+++ one.ts
@@ -4,6 +4,6 @@ let decimal: number = 6;
 let hex: number = 0xf00d;
 let binary: number = 0b1010;
 let octal: number = 0o744;
-let big: bigint = 100n;
+//let big: bigint = 100n;
 
 console.log("Hello TypeScript!");
